a7efdb54a66593a72991a4af229bb9cb98fbe9c0
Lagus645 <lagusu164@gmail.com>
2025-03-22 20:41:37 +0500

    Add files via upload

---
+++ BinSearch/BinSearch/BinSearch.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <clocale>
+#include <ctime>
+#include <stdlib.h>
+using namespace std;
+
+void printArray(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+// Функция сортировки Шелла
+void shellSort(int arr[], int size) {
+    int gap = size / 2;  // Начальный шаг
+
+    // Пока шаг больше 0
+    while (gap > 0) {
+        // Применяем сортировку вставками для текущего шага
+        for (int i = gap; i < size; i++) {
+            int temp = arr[i];
+            int j;
+            // Сдвигаем элементы, пока не найдем правильное место для arr[i]
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+
+        gap /= 2;  // Уменьшаем шаг
+    }
+}
+
+bool BinSearch(int arr[], int st, int end, int size, int k) {
+    int mid = ((st+end)/2);
+    while (arr[mid] != k && mid > 1) {
+        mid = (st + end) / 2;
+        if (((st+end)/2) > k) {
+            st = mid;
+        }
+        else {
+            end = mid;
+        }
+        if (arr[mid] == k) {
+            return true;
+        }
+    }
+    return false;
+    
+}
+
+
+int main(){
+	setlocale(LC_ALL, "russian");
+	srand(time(0));
+	const int n = 10;
+    int k, st = 0, end = n;
+	int m[n];
+	for (int i = 0; i < n; i++) {
+		m[i] = rand() % 100;
+	}
+    cout << "Какое число искать???" << endl;
+    cin >> k;
+    shellSort(m, n);
+	cout << "Массив данных: ";
+	printArray(m, n);
+    cout << BinSearch(m, st, end, n, k);
+    return 0;
+}
---
+++ LinSearch/LinSearch/LinSearch.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <clocale>
+#include <ctime>
+#include <stdlib.h>
+using namespace std;
+
+void printArray(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int linSearch(int arr[], int size, int k) {
+	for (int i = 0; i < size; i++) {
+		if (arr[i] == k) {
+			return i+1;
+		}
+	}
+	return -1;
+}
+
+int main() {
+	setlocale(LC_ALL, "russian");
+	srand(time(0));
+	int k;
+	const int n = 100;
+	int m[n];
+	for (int i = 0; i < n; i++) {
+		m[i] = rand() % 100;
+	}
+	cout << "Массив данных: ";
+	printArray(m, n);
+	cout << "Введите искомое число: ";
+	cin >> k;
+	cout << linSearch(m, n, k);
+	return 0;
+}
